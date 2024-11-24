0c41ea1b84e342a4bd6d2e720f704c9356422edf
KadoProG <splatrain@gmail.com>
2024-11-25 08:00:20 +0900

    fix: title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Taskhub System Admin Web</title>
+    <title>これはタイトル</title>
     @viteReactRefresh
     @vite(['resources/ts/app.tsx'])
 </head>
