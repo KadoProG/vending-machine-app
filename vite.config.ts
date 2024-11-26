8d28971897c89d48ba95efd0649dfa7022664c8f
KadoProG <splatrain@gmail.com>
2024-11-26 13:03:57 +0900

    update: format2

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import react from "@vitejs/plugin-react-swc";
-import laravel from "laravel-vite-plugin";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/ts/app.tsx"],
-            refresh: true,
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./resources/ts"),
-        },
+  plugins: [
+    laravel({
+      input: ['resources/ts/app.tsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/ts'),
     },
+  },
 });
